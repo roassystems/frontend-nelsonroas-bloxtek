bbec4011b774923e371c6b6224f39f23a1a2e355
Nelson Roas <nroas@conexia.com>
2025-02-07 12:14:16 -0400

    Estableciendo cabeceras de seguridad OWASP e evitando ataques de sitios cruzados

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
+          { key: "X-Frame-Options", value: "DENY" },
+          { key: "Cross-Origin-Resource-Policy", value: "cross-origin" },
+          { key: "Content-Security-Policy", value: `${process.env.NEXT_PUBLIC_API_URL} default-src 'self'; img-src 'self' data:; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net;` }
+        ],
+      },
+    ];
+  },
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
