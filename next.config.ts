343947793991d92c304a0f3b85532b8496babedc
Nelson Roas <nroas@conexia.com>
2025-02-07 12:35:38 -0400

    sumando cabeceras de seguridad OWASP

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig = {
           { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
           { key: "X-Frame-Options", value: "DENY" },
           { key: "Cross-Origin-Resource-Policy", value: "cross-origin" },
-          { key: "Content-Security-Policy", value: `${process.env.NEXT_PUBLIC_API_URL} default-src 'self'; img-src 'self' data:; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net;` }
+          { key: "Content-Security-Policy", value: `${process.env.NEXT_PUBLIC_API_URL} default-src 'self'; img-src 'self' data:; script-src 'self' 'unsafe-inline'; style-src 'self' font-src 'self'  'unsafe-inline' https://cdn.jsdelivr.net;` }
         ],
       },
     ];
