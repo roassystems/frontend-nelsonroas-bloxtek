e7075bc06aaaad8f025d3028e8fd3750b18a3dca
Nelson Roas <nroas@conexia.com>
2025-02-20 14:16:07 -0400

    nueva implementacion lectura variable ambiente del lado servidor

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,24 +1,11 @@
 import axios from "axios";
+import { API_URL } from "./config"; // Importamos la URL desde config.ts
 
-// Función para crear la instancia de Axios con baseURL dinámica
-export const createApiInstance = (baseURL) => {
-  return axios.create({
-    baseURL, // Se recibe la URL desde el servidor
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-};
+const api = axios.create({
+  baseURL: API_URL, // Ahora la URL viene del servidor
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
-export async function getServerSideProps() {
-  // Leer la variable del entorno en el servidor
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL || "https://backend-nelsonroas-bloxtek-production.up.railway.app";
-  console.log("valor de api url del lado del servidor "+process.env.NEXT_PUBLIC_API_URL);
-  return {
-    props: {
-      apiUrl, // Pasamos la URL como prop al cliente
-    },
-  };
-}
-const api = createApiInstance(getServerSideProps());
 export default api;
