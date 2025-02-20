1c834a9ed8aa6ccc656dd484ee39f9d5bcc7c82f
Nelson Roas <nroas@conexia.com>
2025-02-20 09:35:30 -0400

    next enviroment variable

---
+++ next.config.mjs
@@ -0,0 +1,47 @@
+// @ts-check
+ 
+/**
+ * @type {import('next').NextConfig}
+ */
+
+const nextConfig = {
+  /* config options here */
+  images: {
+    unoptimized: true,
+    formats:['image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'webapp-spotly-img.s3.us-west-1.amazonaws.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'https://frontend-nelsonroas-bloxtek-production.up.railway.app/'
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com'
+      },
+      
+    ],
+    
+  },
+  devIndicators: {
+    buildActivity: false
+  },
+  trailingSlash: true,
+  env: {
+    NEXT_PUBLIC_API_URL:process.env.NEXT_PUBLIC_API_URL
+  },
+  
+}
+ 
+export default nextConfig
